bda9e43bd54efcd9335946630fdf11e769411709
bwkam <userdev987@gmail.com>
2024-02-17 02:27:06 +0200

    add neorg input (right file this time)

--- flake.nix
+++ flake.nix
@@ -14,6 +14,8 @@
     nurpkgs.url = "github:nix-community/NUR";
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+        neorg-overlay.url = "github:nvim-neorg/nixpkgs-neorg-overlay";
+
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
