53c990c94f8a2e09e4fd231391683ba286749e9a
bwkam <userdev987@gmail.com>
2024-02-18 15:48:02 +0200

    fix name

--- home/modules/editors/neovim/lua/user/dashboard.lua
+++ home/modules/editors/neovim/lua/user/dashboard.lua
@@ -4,75 +4,73 @@ local dashboard = require("dashboard")
 
 local conf = {}
 conf.header = {
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-  "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-  "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-  "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-  "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-  "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-  "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-  " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-  " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-  "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-  "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-  "                                   ",
+	"                                   ",
+	"                                   ",
+	"                                   ",
+	"                                   ",
+	"                                   ",
+	"                                   ",
+	"                                   ",
+	"                                   ",
+	"                                   ",
+	"   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+	"    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+	"          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+	"           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+	"          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+	"   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+	"  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+	" ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+	" ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+	"      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+	"       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+	"                                   ",
 }
 
 conf.center = {
-  {
-    icon = '  ',
-    desc = 'Find File                               ',
-    action = 'Telescope find_files find_command=rg,--hidden,--files',
-    key = 'SPC f f'
-  },
-  {
-    icon = '󱋢  ',
-    desc = 'Recently opened                         ',
-    action = 'Telescope oldfiles',
-    key = 'SPC f r'
-  },
-  {
-    icon = '  ',
-    desc = 'File browser                            ',
-    action = 'NvimTreeToggle',
-    key = 'SPC e'
-  },
-  {
-    icon = '󱌯 ',
-    desc = 'Search for word                         ',
-    action = 'Telescope live_grep',
-    key = 'SPC F'
-  },
-  {
-    icon = '󰋖  ',
-    desc = 'Help                                    ',
-    action = 'Telescope help_tags',
-    key = 'SPC f h'
-  },
+	{
+		icon = "  ",
+		desc = "Find File                               ",
+		action = "Telescope find_files find_command=rg,--hidden,--files",
+		key = "SPC f f",
+	},
+	{
+		icon = "󱋢  ",
+		desc = "Recently opened                         ",
+		action = "Telescope oldfiles",
+		key = "SPC f r",
+	},
+	{
+		icon = "  ",
+		desc = "File browser                            ",
+		action = "NvimTreeToggle",
+		key = "SPC e",
+	},
+	{
+		icon = "󱌯 ",
+		desc = "Search for word                         ",
+		action = "Telescope live_grep",
+		key = "SPC F",
+	},
+	{
+		icon = "󰋖  ",
+		desc = "Help                                    ",
+		action = "Telescope help_tags",
+		key = "SPC f h",
+	},
 }
 
 dashboard.setup({
-  theme = 'doom',
-  shortcut_type = 'number',
-  config = conf
+	theme = "doom",
+	shortcut_type = "number",
+	config = conf,
 })
 
 api.nvim_create_autocmd("FileType", {
-  pattern = "dashboard",
-  group = api.nvim_create_augroup("dashboard_enter", { clear = true }),
-  callback = function()
-    keymap.set("n", "q", ":qa<CR>", { buffer = true, silent = true })
-    keymap.set("n", "e", ":enew<CR>", { buffer = true, silent = true })
-  end
+	pattern = "dashboard",
+	group = api.nvim_create_augroup("dashboard_enter", { clear = true }),
+	callback = function()
+		keymap.set("n", "q", ":qa<CR>", { buffer = true, silent = true })
+		keymap.set("n", "e", ":enew<CR>", { buffer = true, silent = true })
+	end,
 })
--- home/modules/editors/neovim/lua/user/neorg.lua
+++ home/modules/editors/neovim/lua/user/neorg.lua
@@ -1,17 +1,18 @@
 require("neorg").setup({
-	load = {
-		["core.defaults"] = {},
-		["core.dirman"] = {
-			config = {
-				workspaces = {
-					work = "~/notes/refile",
-					semester_2 = "~/notes/semester-2",
-					life = "~/notes/life",
-				},
-			},
-		},
-		["core.concealer"] = {},
-		["core.integrations.image"] = {},
-		["core.latex.renderer"] = {},
-	},
+  load = {
+    ["core.defaults"] = {},
+    ["core.dirman"] = {
+      config = {
+        workspaces = {
+          work = "~/notes/work",
+          refile = "~/notes/refile",
+          semester_2 = "~/notes/semester-2",
+          life = "~/notes/life",
+        },
+      },
+    },
+    ["core.concealer"] = {},
+    ["core.integrations.image"] = {},
+    ["core.latex.renderer"] = {},
+  },
 })
