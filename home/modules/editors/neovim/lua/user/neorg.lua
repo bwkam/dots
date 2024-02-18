9c8ad76f2262600cb1fa75797b8fd192aad2adb7
bwkam <userdev987@gmail.com>
2024-02-18 13:39:59 +0200

    add integrations.image and latex.renderer (neorg)

--- home/modules/editors/neovim/lua/user/neorg.lua
+++ home/modules/editors/neovim/lua/user/neorg.lua
@@ -1,14 +1,17 @@
 require("neorg").setup({
-  load = {
-    ["core.defaults"] = {},
-    ["core.dirman"] = {
-      config = {
-        workspaces = {
-          work = "~/notes/work",
-          home = "~/notes/home",
-        },
-      },
-    },
-    ["core.concealer"] = {},
-  },
+	load = {
+		["core.defaults"] = {},
+		["core.dirman"] = {
+			config = {
+				workspaces = {
+					work = "~/notes/refile",
+					semester_2 = "~/notes/semester-2",
+					life = "~/notes/life",
+				},
+			},
+		},
+		["core.concealer"] = {},
+		["core.integrations.image"] = {},
+		["core.latex.renderer"] = {},
+	},
 })
