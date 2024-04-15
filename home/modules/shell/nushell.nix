5f257054a4a7433e1ad443ad112f1b903de0493c
bwkam <userdev987@gmail.com>
2024-04-15 22:45:01 +0200

    nushell: set show_banner, and algo to fuzzy

--- home/modules/shell/nushell.nix
+++ home/modules/shell/nushell.nix
@@ -8,12 +8,18 @@ in {
 
       enable = true;
 
+      extraConfig = ''
+        $env.config = {
+          show_banner: false,
+           algorithm: "fuzzy"
+        }
+      '';
+
       shellAliases = with pkgs; {
         ".." = "cd ..";
         cat = "${bat}/bin/bat";
         ls = "${eza}/bin/eza --group-directories-first --git --icons";
         ytmp3 = "yt-dlp --extract-audio --audio-format mp3";
-        haxe_language_server = "node $HOME/haxe-language-server/bin/server.js";
         v = "nvim";
       };
 
