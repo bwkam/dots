7f691b6c6bb7fa421f06c4a9187f89de057c713f
bwkam <userdev987@gmail.com>
2024-04-22 02:43:28 +0200

    Revert "Revert "neovim: try stable again""
    This reverts commit 862ce72563b2162bba378c3e8d5dfb8c098878b1.

--- home/modules/editors/neovim/lua/user/lualine.lua
+++ home/modules/editors/neovim/lua/user/lualine.lua
@@ -1,7 +1,7 @@
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "pywal16-nvim",
+		-- theme = "pywal16-nvim",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
