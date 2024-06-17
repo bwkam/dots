dbf085e63c0b647230d7f7af1d38ac53ce88544b
bwkam <userdev987@gmail.com>
2024-06-17 15:42:18 +0300

    don't need any overlays for now

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,17 +1,2 @@
 {...}: (_: prev: {
-  pywal = prev.python310Packages.buildPythonPackage {
-    pname = "pywal";
-    version = "3.5.0";
-    propogatedBuildInputs = with prev; [
-      imagemagick
-      feh
-      python310Packages.colorthief
-      colorz
-    ];
-    src = builtins.fetchGit {
-      url = "https://github.com/eylles/pywal16";
-      rev = "806d0268a75eb17d1bc60ad81f00500e56097d3d";
-    };
-    doCheck = false;
-  };
 })
